35a8d7a8b3dc5f5ac57b5b5d491a3fc34babf7b1
Hubert Grędziński <hubert.gredzinski@gmail.com>
2022-03-19 09:57:11 +0100

    Add bloc

---
+++ lib/features/home/pages/cubit/restaurants_cubit.dart
@@ -0,0 +1,8 @@
+import 'package:bloc/bloc.dart';
+import 'package:meta/meta.dart';
+
+part 'restaurants_state.dart';
+
+class RestaurantsCubit extends Cubit<RestaurantsState> {
+  RestaurantsCubit() : super(RestaurantsState());
+}
---
+++ lib/features/home/pages/cubit/restaurants_state.dart
@@ -0,0 +1,4 @@
+part of 'restaurants_cubit.dart';
+
+@immutable
+class RestaurantsState {}
