6bfce746144fce071fbeb35d561d8089ab9affaf
Hubert Grędziński <hubert.gredzinski@gmail.com>
2022-04-05 09:53:51 +0200

    Add home cubbit and state

--- lib/features/home/cubit/home_cubit.dart
+++ lib/features/home/cubit/home_cubit.dart
@@ -1,7 +1,76 @@
+import 'dart:async';
 import 'package:bloc/bloc.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:meta/meta.dart';
+import 'package:flutter/material.dart';
 part 'home_state.dart';
 
 class HomeCubit extends Cubit<HomeState> {
-  HomeCubit() : super(HomeState());
+  HomeCubit()
+      : super(
+          const HomeState(
+            documents: [],
+            errorMessage: '',
+            isLoading: false,
+          ),
+        );
+
+  StreamSubscription? _streamSubscription;
+
+  Future<void> start() async {
+    emit(
+      const HomeState(
+        documents: [],
+        errorMessage: '',
+        isLoading: true,
+      ),
+    );
+
+    _streamSubscription = FirebaseFirestore.instance
+        .collection('goal')
+        .snapshots()
+        .listen((data) {
+      emit(
+        HomeState(
+          documents: data.docs,
+          isLoading: false,
+          errorMessage: '',
+        ),
+      );
+    })
+      ..onError((error) {
+        emit(
+          HomeState(
+            documents: const [],
+            isLoading: false,
+            errorMessage: error.toString(),
+          ),
+        );
+      });
+  }
+
+  Future<void> remove({required String documentID}) async {
+    try {
+      await FirebaseFirestore.instance
+          .collection('goal')
+          .doc(documentID)
+          .delete();
+    } catch (error) {
+      emit(
+        HomeState(
+          documents: const [],
+          isLoading: false,
+          errorMessage: error.toString(),
+        ),
+      );
+      start();
+    }
+  }
+
+  @override
+  Future<void> close() {
+    _streamSubscription?.cancel();
+    return super.close();
+  }
 }
+
--- lib/features/home/cubit/home_state.dart
+++ lib/features/home/cubit/home_state.dart
@@ -1,4 +1,14 @@
 part of 'home_cubit.dart';
 
 @immutable
-class HomeState {}
+class HomeState {
+  final List<QueryDocumentSnapshot<Map<String, dynamic>>> documents;
+  final bool isLoading;
+  final String errorMessage;
+
+  const HomeState({
+    required this.documents,
+    required this.isLoading,
+    required this.errorMessage,
+  });
+}
