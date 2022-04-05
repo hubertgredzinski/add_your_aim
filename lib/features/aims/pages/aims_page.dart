f8438eee25785dd6817bd840b6f4d904ee8c6a56
Hubert Grędziński <hubert.gredzinski@gmail.com>
2022-04-05 09:55:24 +0200

    Add ListView and Textfield of title

--- lib/features/aims/pages/aims_page.dart
+++ lib/features/aims/pages/aims_page.dart
@@ -1,99 +1,92 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moja_apka/features/aims/pages/cubit/aims_cubit.dart';
-import 'package:moja_apka/features/auth/pages/user_profile.dart';
+import 'package:moja_apka/features/aims/pages/cubit/aims_state.dart';
 
-class AimsPage extends StatelessWidget {
+class AimsPage extends StatefulWidget {
   const AimsPage({
     Key? key,
   }) : super(key: key);
 
   @override
+  State<AimsPage> createState() => _AimsPageState();
+}
+
+class _AimsPageState extends State<AimsPage> {
+  String? _title;
+
+  @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: const Color.fromARGB(226, 236, 236, 236),
-      appBar: AppBar(
-        title: const Center(
-          child: Text('Cele'),
-        ),
-        actions: [
-          IconButton(
-            onPressed: () {
-              Navigator.of(context).push(
-                MaterialPageRoute(
-                  builder: (context) => const UserProfile(),
-                ),
-              );
-            },
-            icon: const Icon(Icons.person),
-          ),
-        ],
-      ),
-      body: BlocProvider(
-        create: (context) => AimsCubit()..start(),
-        child: BlocBuilder<AimsCubit, AimsState>(
-          builder: (context, state) {
-            state.documents;
-            return StreamBuilder<QuerySnapshot>(
-              builder: (context, snapshot) {
-                if (state.errorMessage.isNotEmpty) {
-                  return Center(
-                      child: Text('Wystąpił błąd : ${state.errorMessage}'));
-                }
-
-                if (state.isLoading) {
-                  return const Center(child: CircularProgressIndicator());
-                }
-
-                final documents = state.documents;
-
-                return ListView(
-                  children: [
-                    for (final document in documents) ...[
-                      Dismissible(
-                        key: ValueKey(document.id),
-                        onDismissed: (_) {
-                          FirebaseFirestore.instance
-                              .collection('goal')
-                              .doc(document.id)
-                              .delete();
-                        },
-                        child: AimCategory(
-                          document['title'],
-                        ),
-                      ),
-                    ],
-                  ],
-                );
-              },
+    return BlocProvider(
+      create: (context) => AimsCubit(),
+      child: BlocBuilder<AimsCubit, AimsState>(
+        builder: (context, state) {
+          if (state.errorMessage.isNotEmpty) {
+            return Center(
+              child: Text('Wystąpił błąd : ${state.errorMessage}'),
             );
-          },
-        ),
+          }
+
+          if (state.isLoading) {
+            return const Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          return Scaffold(
+            appBar: AppBar(
+              title: const Center(child: Text('Dodaj cel')),
+              actions: [
+                IconButton(
+                    onPressed: _title == null
+                        ? null
+                        : () {
+                            context.read<AimsCubit>().add(_title!);
+                          },
+                    icon: const Icon(Icons.check))
+              ],
+            ),
+            body: _AimsPageBody(
+              onTitleChanged: (newValue) {
+                setState(() {
+                  _title = newValue;
+                });
+              },
+            ),
+          );
+        },
       ),
     );
   }
 }
 
-class AimCategory extends StatelessWidget {
-  const AimCategory(
-    this.title, {
+class _AimsPageBody extends StatelessWidget {
+  const _AimsPageBody({
     Key? key,
+    required this.onTitleChanged,
   }) : super(key: key);
 
-  final String title;
+  final Function(String) onTitleChanged;
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Center(
-        child: Text(
-          title,
+    return ListView(
+      padding: const EdgeInsets.all(30),
+      children: [
+        TextField(
+          decoration: const InputDecoration(
+            labelText: 'Aktywność',
+            enabledBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.black, width: 1),
+            ),
+            focusedBorder: OutlineInputBorder(
+              borderSide: BorderSide(color: Colors.green, width: 2),
+            ),
+          ),
+          onChanged: onTitleChanged,
         ),
-      ),
-      color: Colors.amber,
-      padding: const EdgeInsets.all(20),
-      margin: const EdgeInsets.all(10),
+        const SizedBox(height: 20),
+      ],
     );
   }
 }
