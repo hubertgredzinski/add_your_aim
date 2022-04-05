616a1e8eed267f593645d5752bbbe7a993f4dd29
Hubert Grędziński <hubert.gredzinski@gmail.com>
2022-04-05 09:53:22 +0200

    Change Navigation Bar to FloatingAction  Button

--- lib/features/home/pages/home_page.dart
+++ lib/features/home/pages/home_page.dart
@@ -1,46 +1,114 @@
 import 'package:flutter/material.dart';
-import 'package:moja_apka/features/add_aim/pages/add_aim_page.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:moja_apka/features/aims/pages/aims_page.dart';
-import 'package:moja_apka/features/history/pages/history_pages.dart';
+import 'package:moja_apka/features/auth/pages/user_profile.dart';
+import 'package:moja_apka/features/home/cubit/home_cubit.dart';
 
-class HomePage extends StatefulWidget {
+class HomePage extends StatelessWidget {
   const HomePage({
     Key? key,
   }) : super(key: key);
 
-  @override
-  State<HomePage> createState() => _HomePageState();
-}
 
-class _HomePageState extends State<HomePage> {
-  int currrentIndex = 0;
-  final screens = [
-    const AimsPage(),
-     AddAimPage(),
-    const HistoryPage(),
-  ];
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: IndexedStack(
-        index: currrentIndex,
-        children: screens,
-      ),
-      bottomNavigationBar: BottomNavigationBar(
-        type: BottomNavigationBarType.fixed,
-        backgroundColor: Colors.white,
-        selectedItemColor: Colors.black,
-        unselectedItemColor: Colors.grey,
-        iconSize: 30,
-        currentIndex: currrentIndex,
-        onTap: (index) => setState(() => currrentIndex = index),
-        items: const [
-          BottomNavigationBarItem(icon: Icon(Icons.toc), label: 'Cele'),
-          BottomNavigationBarItem(icon: Icon(Icons.add), label: 'Dodaj cel'),
-          BottomNavigationBarItem(
-              icon: Icon(Icons.assignment), label: 'Historia'),
+      backgroundColor: Colors.white,
+      appBar: AppBar(
+        title: const Center(
+          child: Text('Cele'),
+        ),
+        actions: [
+          IconButton(
+            onPressed: () {
+              Navigator.of(context).push(
+                MaterialPageRoute(
+                  builder: (context) => const UserProfile(),
+                ),
+              );
+            },
+            icon: const Icon(Icons.person),
+          ),
         ],
       ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              fullscreenDialog: true,
+              builder: (context) => const AimsPage(),
+            ),
+          );
+        },
+        child: const Icon(Icons.add),
+      ),
+      body: BlocProvider(
+        create: (context) => HomeCubit()..start(),
+        child: BlocBuilder<HomeCubit, HomeState>(
+          builder: (context, state) {
+            state.documents;
+            if (state.errorMessage.isNotEmpty) {
+              return Center(
+                  child: Text('Wystąpił błąd : ${state.errorMessage}'));
+            }
+
+            if (state.isLoading) {
+              return const Center(child: CircularProgressIndicator());
+            }
+
+            final documents = state.documents;
+
+            return ListView(
+              children: [
+                for (final document in documents) ...[
+                  Dismissible(
+                    key: ValueKey(document.id),
+                    background: const DecoratedBox(
+                      decoration: BoxDecoration(color: Colors.red),
+                      child: Align(
+                        alignment: Alignment.centerRight,
+                        child: Padding(
+                          padding: EdgeInsets.all(20.0),
+                          child: Icon(Icons.delete),
+                        ),
+                      ),
+                    ),
+                    onDismissed: (direction) {
+                      context.read<HomeCubit>().remove(documentID: document.id);
+                    },
+                    child: AimCategory(
+                      document['title'],
+                    ),
+                  ),
+                ],
+              ],
+            );
+          },
+        ),
+      ),
+    );
+  }
+}
+
+class AimCategory extends StatelessWidget {
+  const AimCategory(
+    this.title, {
+    Key? key,
+  }) : super(key: key);
+
+  final String title;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Center(
+        child: Text(
+          title,
+        ),
+      ),
+      color: Colors.amber,
+      padding: const EdgeInsets.all(20),
+      margin: const EdgeInsets.all(10),
     );
   }
 }
