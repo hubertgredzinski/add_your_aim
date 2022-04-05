4037b06b9d4faa506f94d222ace771614174ba1e
Hubert Grędziński <hubert.gredzinski@gmail.com>
2022-04-05 09:54:17 +0200

    Add aims cubit and state

--- lib/features/aims/pages/cubit/aims_cubit.dart
+++ lib/features/aims/pages/cubit/aims_cubit.dart
@@ -1,51 +1,35 @@
 import 'dart:async';
-
 import 'package:bloc/bloc.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
-import 'package:meta/meta.dart';
-
-part 'aims_state.dart';
+import 'aims_state.dart';
 
 class AimsCubit extends Cubit<AimsState> {
   AimsCubit()
       : super(
           const AimsState(
-            documents: [],
             errorMessage: '',
             isLoading: false,
           ),
         );
 
-        StreamSubscription? _streamSubscription;
+  StreamSubscription? _streamSubscription;
 
-  Future<void> start() async {
-    emit(
-      const AimsState(
-        documents: [],
-        errorMessage: '',
-        isLoading: true,
-      ),
-    );
-
-   _streamSubscription = FirebaseFirestore.instance.collection('goal').snapshots().listen((data) {
+  Future<void> add(
+    String title,
+  ) async {
+    try {
+      await FirebaseFirestore.instance.collection('goal').add({
+        'title': title,
+      });
       emit(
-        AimsState(
-          documents: data.docs,
-          isLoading: false,
-          errorMessage: '',
-        ),
+        const AimsState(isLoading: false, errorMessage: ''),
       );
-    })..onError((error) {
-     emit(
-        AimsState(
-          documents: const [],
-          isLoading: false,
-          errorMessage: error.toString(),
-        ),
-      );
-    });
+    } catch (error) {
+      AimsState(isLoading: false, errorMessage: error.toString());
+    }
   }
-   @override
+
+  @override
   Future<void> close() {
     _streamSubscription?.cancel();
     return super.close();
--- lib/features/aims/pages/cubit/aims_state.dart
+++ lib/features/aims/pages/cubit/aims_state.dart
@@ -1,13 +1,8 @@
-part of 'aims_cubit.dart';
-
-@immutable
 class AimsState {
-  final List<QueryDocumentSnapshot<Object?>> documents;
   final bool isLoading;
   final String errorMessage;
-
+  
   const AimsState({
-    required this.documents,
     required this.isLoading,
     required this.errorMessage,
   });
